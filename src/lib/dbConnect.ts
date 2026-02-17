2380b06ed535125607135019be8a6f9bae5f47d1
Ataullah Mesbah <mesbahataullah4@gmail.com>
2026-02-17 21:26:16 +0600

    added issue dns solve

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,5 +1,8 @@
 // lib/dbConnect.ts
 import mongoose from 'mongoose';
+import dns from 'node:dns/promises';
+
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 
 // Interface for cached connection
 interface MongooseCache {
